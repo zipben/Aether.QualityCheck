f73de84d647b44ccdbcc88a6488c5cb79e230b19
Andy Kratzert <andykratzert@quickenloans.com>
2020-12-01 14:40:46 -0500

    Putting lower case 'd' in RMClientId, RMLoanId, RocketAccountId so it will match current data

--- Aether/Enums/IdentifierType.cs
+++ Aether/Enums/IdentifierType.cs
@@ -4,9 +4,9 @@
     {
         GCID = 1,
         LoanNumber = 2,
-        RMClientID = 3,
-        RMLoanID = 4,
-        RocketAccountID = 5,
+        RMClientId = 3,
+        RMLoanId = 4,
+        RocketAccountId = 5,
         PhoneNumber = 6
     }
 }
