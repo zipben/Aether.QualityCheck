ec290bf014903c9a6384e5691a66d55c63395d96
Andy Kratzert <andykratzert@quickenloans.com>
2020-12-04 12:11:37 -0500

    Adding Enumerable extensions

---
+++ Aether/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Aether.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool HasMoreThanOneItem<T>(this IEnumerable<T> enumeration) =>
+            enumeration != null && enumeration.Any() && enumeration.Skip(1).Any();
+    }
+}
