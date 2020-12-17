dd7d0f6d870f819fd09b7214ce839c84f02672a5
Andy Kratzert <andykratzert@quickenloans.com>
2020-12-17 16:43:26 -0500

    Fixing Enforcement Response

--- Aether/Models/EnforcementResponse.cs
+++ Aether/Models/EnforcementResponse.cs
@@ -5,8 +5,13 @@ namespace Aether.Models
 {
     public class EnforcementResponse
     {
+        public string EnforcementRequestId { get; set; }
+
         /// The following is NOT set in enforcement responses for right to delete instead, these are classifications by-name 
         [JsonProperty("data")]
         public List<Classification> Data { get; set; } = new List<Classification>();
+
+        // Indicates we patch to the test endpoint
+        public bool IsTestMessage { get; set; }
     }
 }
