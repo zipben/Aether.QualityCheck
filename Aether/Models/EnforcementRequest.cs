913602f653ec8df03493fd68a36adeba37c87eae
EAubrey <EmmanuelAubrey@quickenloans.com>
2020-12-23 16:47:50 -0500

    nullable enum enforementRequest

--- Aether/Models/EnforcementRequest.cs
+++ Aether/Models/EnforcementRequest.cs
@@ -9,7 +9,7 @@ namespace Aether.Models
 
         public Dictionary<string, List<string>> Identifiers { get; set; }
 
-        public EnforcementType EnforcementType { get; set; }
+        public EnforcementType? EnforcementType { get; set; }
 
         // Was DRE able to determine that QL has the SSN of the individual who made the request.
         // Default to true so that if DRE is not yet sending this field then we won't default to false
