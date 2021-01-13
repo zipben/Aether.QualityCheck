eba51236a41296c30fa7b23e1fd038dacdd4482c
Andy Kratzert <andykratzert@quickenloans.com>
2021-01-13 12:55:51 -0500

    Adding Enforcement Action Type Enum and IsNullOrEmpty extension

---
+++ Aether/Enums/EnforcementActionType.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Aether.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum EnforcementActionType
+    {
+        ActionTaken = 1,
+        ActionNotTaken = 2,
+        NotFound = 3
+    }
+}
