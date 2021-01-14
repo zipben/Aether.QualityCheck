f5ff02090794282e19d04737d16c7200108a166d
EAubrey <EmmanuelAubrey@quickenloans.com>
2021-01-14 16:51:38 -0500

    adding enforcement enum

--- Aether/Interfaces/IEnforcementMessage.cs
+++ Aether/Interfaces/IEnforcementMessage.cs
@@ -1,7 +1,10 @@
-﻿namespace Aether.Interfaces
+﻿using Aether.Enums;
+
+namespace Aether.Interfaces
 {
     public interface IEnforcementMessage
     {
         public string EnforcementRequestId { get; set; }
+        public EnforcementType EnforcementType { get; set; }
     }
 }
