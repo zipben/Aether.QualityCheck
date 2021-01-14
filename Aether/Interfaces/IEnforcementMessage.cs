4d092bfa4620547937a7def8c373a93d29fb42b9
EAubrey <EmmanuelAubrey@quickenloans.com>
2021-01-14 16:54:22 -0500

    nullable

--- Aether/Interfaces/IEnforcementMessage.cs
+++ Aether/Interfaces/IEnforcementMessage.cs
@@ -5,6 +5,6 @@ namespace Aether.Interfaces
     public interface IEnforcementMessage
     {
         public string EnforcementRequestId { get; set; }
-        public EnforcementType EnforcementType { get; set; }
+        public EnforcementType? EnforcementType { get; set; }
     }
 }
