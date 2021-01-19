863826263f6337a113f922de69a292384e582573
Deepti Keshavamurthy <deeptikeshavamurthy@quickenloans.com>
2021-01-19 11:34:11 -0500

    Adding interface for system lookup model.

---
+++ Aether/Interfaces/ISystemLookUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aether.Interfaces
+{
+    public interface ISystemLookUp
+    {
+        string SystemName { get; set; }
+        string EmailAddress { get; set; }
+        string CreatedDate { get; set; }
+        string LastUpdatedDate { get; set; }
+    }
+}
