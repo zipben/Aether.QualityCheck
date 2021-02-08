10fd6738885fb3cb7fc91e6945cbcde2d5f2643c
Andy Kratzert <andykratzert@quickenloans.com>
2021-02-08 14:20:41 -0500

    Adding LongDate Extensions

---
+++ Aether/Extensions/LongDateExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Oya_UI.Extensions
+{
+    public static class LongDateExtensions
+    {
+        public static DateTime SecondsToDate(this long val) =>
+            DateTimeOffset.FromUnixTimeSeconds(val).DateTime;
+
+        public static DateTime? SecondsToDate(this long? val) =>
+            val.HasValue 
+                ? val.Value.SecondsToDate()
+                : (DateTime?) null;
+
+        public static DateTime MillisecondsToDate(this long val) =>
+            DateTimeOffset.FromUnixTimeMilliseconds(val).DateTime;
+
+        public static DateTime? MillisecondsToDate(this long? val) =>
+            val.HasValue
+                ? val.Value.MillisecondsToDate()
+                : (DateTime?) null;
+    }
+}
