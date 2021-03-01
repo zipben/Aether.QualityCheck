898c665fb59285299c58ea759044ce426cebe162
Andy Kratzert <andykratzert@quickenloans.com>
2021-03-01 12:17:22 -0500

    Extensions to add security headers to requests

--- Aether/Extensions/ApplicationBuilderExtensions.cs
+++ Aether/Extensions/ApplicationBuilderExtensions.cs
@@ -14,5 +14,26 @@ namespace Aether.Extensions
             builder.UseMiddleware<ErrorHandlingMiddleware>();
         public static IApplicationBuilder UseGrafanaControllerMiddleware(this IApplicationBuilder builder) =>
             builder.UseMiddleware<GrafanaControllersMiddleware>();
+
+        /// <summary>
+        /// Adds Strict Transport Security and XSS Protection headers to responses
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="isDevelopment"></param>
+        public static void UseSecurityHeaders(this IApplicationBuilder builder, bool isDevelopment)
+        {
+            if (!isDevelopment)
+            {
+                builder.UseHsts();
+            }
+
+            builder.UseHttpsRedirection();
+
+            builder.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Xss-Protection", "1");
+                await next();
+            });
+        }
     }
 }
---
+++ Aether/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aether.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Enables Strict Transport Security header
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddSecurityHeaders(this IServiceCollection services)
+        {
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(365);
+            });
+        }
+    }
+}
