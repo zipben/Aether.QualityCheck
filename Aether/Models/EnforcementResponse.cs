a5edefa5e6d27314d6aa225cecb511d83d43a911
Benjamin Donaldson <benjamindonaldson@quickenloans.com>
2021-03-02 14:28:41 -0500

    adding additional field that DRE requires for r2d

--- Aether/Models/EnforcementResponse.cs
+++ Aether/Models/EnforcementResponse.cs
@@ -12,7 +12,7 @@ namespace Aether.Models
         /// The following is NOT set in enforcement responses for right to delete instead, these are classifications by-name 
         [JsonProperty("data")]
         public List<Classification> Data { get; set; } = new List<Classification>();
-
+        public string RequestCloseReason { get; set; }
         // Indicates we patch to the test endpoint
         public bool IsTestMessage { get; set; }
         public EnforcementType? EnforcementType { get; set; }
