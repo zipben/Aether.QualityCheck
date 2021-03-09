984786f610a476960ceb581056fdda1e806cec7a
Benjamin Donaldson <benjamindonaldson@quickenloans.com>
2021-03-09 15:12:26 -0500

    Nice readable names for this Enum, and a static helper to make our enums easy to use in our UIs

--- Aether/Extensions/EnumExtensions.cs
+++ Aether/Extensions/EnumExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Aether.Enums;
+using Aether.Helpers;
 
 namespace Aether.Extensions
 {
@@ -13,18 +14,12 @@ namespace Aether.Extensions
                         EnforcementActionType.ActionTaken => "Identifiers Deleted",
                         EnforcementActionType.ActionNotTaken => "Identifiers Not Deleted",
                         EnforcementActionType.NotFound => "Identifiers Not Found",
+                        EnforcementActionType.ActionPartiallyTaken => "Some Identifiers Deleted",
                         _ => string.Empty,
                     },
                 _ => "N/A"
             };
 
-        public static string GetFriendlyDescription(this EnforcementType type) =>
-            type switch
-            {
-                EnforcementType.RightToAccess => "Right To Access",
-                EnforcementType.RightToDelete => "Right To Delete",
-                EnforcementType.RightToKnow => "Right To Know",
-                _ => string.Empty
-            };
+        public static string GetFriendlyDescription(this EnforcementType type) => EnumHelpers.GetFriendlyName(type);
     }
 }
