630c16be6fe7f1ba9b4d943c3247f52a99d3d104
Benjamin Donaldson <benjamindonaldson@quickenloans.com>
2021-03-17 14:41:37 -0400

    added some static values for things shared between themin ui and backend

---
+++ Aether/ServiceSpecificStaticValues/ThemisStaticValues.cs
@@ -0,0 +1,8 @@
+﻿namespace ServiceSpecificStaticValues.Themis.StaticValues
+{
+    public static class StaticEmailContentKeys
+    {
+        public static string BODY_KEY = "body";
+        public static string CASE_NAME_KEY = "caseName";
+    }
+}
