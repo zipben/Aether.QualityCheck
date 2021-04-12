f82a520f09ea2d7ed1e8f2f4cd8d213952bac1ff
Deepti Keshavamurthy <deeptikeshavamurthy@quickenloans.com>
2021-04-12 10:37:37 -0400

    Adding interface for IdentifierRestriction

---
+++ Aether/Interfaces/IIdentifierRestriction.cs
@@ -0,0 +1,19 @@
+﻿using Aether.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aether.Interfaces
+{
+    public interface IIdentifierRestriction
+    {
+        public string Id { get; set; }
+        public IdentifierType IdentifierType { get; set; }
+        public bool IsRestricted { get; set; }
+        public int CreatedById { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int LastUpdatedById { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+
+    }
+}
