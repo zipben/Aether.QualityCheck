4b4688c8d7e43d12981c77725858566d21c15d94
Benjamin Donaldson <benjamindonaldson@quickenloans.com>
2021-04-16 10:51:20 -0400

    moved custom exception to aether

---
+++ Aether/CustomExtceptions/HealthCheckException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Aether.CustomExceptions
+{
+    public class HealthcheckException : Exception
+    {
+        public HealthcheckException(string message) : base(message) { }
+
+        public HealthcheckException(string message, Exception exception) : base(message, exception) { }
+    }
+}
