7f88dfc481f6d41e252117ad8c5645385d044569
Andy Kratzert <andykratzert@quickenloans.com>
2021-04-23 11:07:01 -0400

    Null check in Make object to log

--- Aether/Extensions/Models/DataElementClassificationExtensions.cs
+++ Aether/Extensions/Models/DataElementClassificationExtensions.cs
@@ -6,16 +6,22 @@ namespace Aether.Extensions
     public static class DataElementClassificationExtensions
     {
         public static object MakeObjectToLog(this IDataElementClassification dataElementClassification) =>
-            new
-            {
-                dataElementClassification.Category,
-                dataElementClassification.Classification,
-                dataElementClassification.Entity,
-                dataElementClassification.HasSSN
-            };
+            dataElementClassification is null ? null
+                                              : new
+                                                {
+                                                    dataElementClassification.Category,
+                                                    dataElementClassification.Classification,
+                                                    dataElementClassification.Entity,
+                                                    dataElementClassification.HasSSN
+                                                };
 
         public static object MakeObjectToLog(this IEnumerable<IDataElementClassification> dataElementClassificationList)
         {
+            if (dataElementClassificationList is null)
+            {
+                return null;
+            }
+
             var list = new List<object>();
             foreach (var item in dataElementClassificationList)
             {
