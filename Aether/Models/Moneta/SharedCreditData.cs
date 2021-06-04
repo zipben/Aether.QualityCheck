a52f1928381b4afc033bd9efa95bfaeff85d7a33
EAubrey <EmmanuelAubrey@quickenloans.com>
2021-06-04 16:42:02 -0400

    Addind Shared Credit Data Model

---
+++ Aether/Models/Moneta/SharedCreditData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aether.Models.Moneta
+{
+    public class SharedCreditData
+    {
+        public string CreditGuid { get; set; }
+        public List<string> GCIDS { get; set; }
+    }
+}
