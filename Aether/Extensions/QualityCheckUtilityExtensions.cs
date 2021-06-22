c6b5f0db8e5490c4315cbefe8ee024696ff0ccfb
Benjamin Donaldson <benjamindonaldson@quickenloans.com>
2021-06-22 15:20:26 -0400

    Added quick helper extension to make converting from old to new QualityCheck framework easier

---
+++ Aether/Extensions/QualityCheckUtilityExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Aether.Models;
+using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aether.Extensions
+{
+    public static class QualityCheckHelperExtensions
+    {
+        public static QualityCheckResponseModel QuickConvertToModel(this bool checkPassed, string checkName, string stepName)
+        {
+            Guard.Against.Null(checkName, nameof(checkName));
+            Guard.Against.Null(stepName, nameof(stepName));
+
+            QualityCheckResponseModel model = new QualityCheckResponseModel(checkName);
+
+            StepResponse step = new StepResponse();
+            step.Name = stepName;
+            step.StepPassed = checkPassed;
+
+            model.Steps.Add(step);
+
+            return model;
+        }
+    }
+}
