426cc5d1caa0e9682564b1a39786f07f717bb1ac
Andy Kratzert <andykratzert@quickenloans.com>
2021-07-01 09:21:14 -0400

    Moving KafkaIdentifier here Moving IIdentifier to shared area since it's used by Themis and Oya

---
+++ Aether/Extensions/Models/IdentifierExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Aether.Interfaces;
+using Aether.Models.Kafka;
+
+namespace Aether.Extensions.Models
+{
+    public static class IdentifierExtensions
+    {
+        public static IEnumerable<KafkaIdentifier> MakeKafkaIdentifiers(this IEnumerable<IIdentifier> identifiers)
+        {
+            foreach (var identifier in identifiers)
+            {
+                yield return identifier.MakeKafkaIdentifier();
+            }
+        }
+
+        public static KafkaIdentifier MakeKafkaIdentifier(this IIdentifier identifier)
+        {
+            return new KafkaIdentifier
+            {
+                Type = identifier.IdentifierType.ToString(),
+                Value = identifier.IdentifierValues
+            };
+        }
+    }
+}
