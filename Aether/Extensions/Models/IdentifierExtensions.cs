68b49f48dbd8d35ab9f09cc248eabf072a1d9d22
Andy Kratzert <andykratzert@quickenloans.com>
2021-07-01 10:50:18 -0400

    Adding another extension

--- Aether/Extensions/Models/IdentifierExtensions.cs
+++ Aether/Extensions/Models/IdentifierExtensions.cs
@@ -6,6 +6,18 @@ namespace Aether.Extensions.Models
 {
     public static class IdentifierExtensions
     {
+        public static IEnumerable<KafkaIdentifier> ToKafkaIdentifiers(this Dictionary<string, List<string>> identifiers)
+        {
+            foreach (var item in identifiers)
+            {
+                yield return new KafkaIdentifier
+                {
+                    Type = item.Key,
+                    Value = item.Value
+                };
+            }
+        }
+
         public static IEnumerable<KafkaIdentifier> ToKafkaIdentifiers(this IEnumerable<IIdentifier> identifiers)
         {
             foreach (var identifier in identifiers)
