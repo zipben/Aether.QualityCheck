3394c9d52131ce46a8da5cd314a044a261e0690e
Andy Kratzert <andykratzert@quickenloans.com>
2021-07-19 12:20:41 -0400

    Missed one model

---
+++ Aether/Models/Identifier.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Aether.Enums;
+using Aether.Interfaces;
+
+namespace Aether.Models
+{
+    public class Identifier: IIdentifier
+    {
+        public IdentifierType IdentifierType { get; set; }
+        public List<string> IdentifierValues { get; set; }
+
+        public Identifier()
+        {
+            IdentifierValues = new List<string>();
+        }
+    }
+}
