877a9ee7e75d5370fd91bf91c2b9a7a7fb474d7e
Andy Kratzert <andykratzert@quickenloans.com>
2021-07-19 10:39:25 -0400

    Spacing

--- Aether/Models/Themis/Litigation.cs
+++ Aether/Models/Themis/Litigation.cs
@@ -8,19 +8,12 @@ namespace Aether.Models.Themis
     public class Litigation : ILitigation
     {
         public string Id { get; set; }
-  
         public string CaseName { get; set; }
-
         public DateTime DateHoldCreated { get; set; }
-
         public DateTime? DateHoldEnded { get; set; }
-
         public List<IIdentifier> InputIdentifiers { get; set; }
-
         public List<IIdentifier> ResolvedIdentifiers { get; set; }
-
         public DateTime LastUpdateDate { get; set; }
-
         public DateTime CreateDate { get; set; }
     }
 }
