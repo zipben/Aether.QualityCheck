094aebb54a3dcd30c53122b510fc2c1674ba446c
Andy Kratzert <andykratzert@quickenloans.com>
2021-07-19 10:38:24 -0400

    Moving three shared models to Aether

---
+++ Aether/Models/Themis/Litigation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Aether.Interfaces;
+using Aether.Interfaces.Themis;
+
+namespace Aether.Models.Themis
+{
+    public class Litigation : ILitigation
+    {
+        public string Id { get; set; }
+  
+        public string CaseName { get; set; }
+
+        public DateTime DateHoldCreated { get; set; }
+
+        public DateTime? DateHoldEnded { get; set; }
+
+        public List<IIdentifier> InputIdentifiers { get; set; }
+
+        public List<IIdentifier> ResolvedIdentifiers { get; set; }
+
+        public DateTime LastUpdateDate { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
