996aedea5a8057ba749af91eebc0d374b835fa1f
Benjamin Donaldson <benjamindonaldson@quickenloans.com>
2021-07-23 11:12:07 -0400

    forgot to update the interface

--- Aether/ExternalAccessClients/Interfaces/IHttpClientWrapper.cs
+++ Aether/ExternalAccessClients/Interfaces/IHttpClientWrapper.cs
@@ -10,11 +10,16 @@ namespace Aether.ExternalAccessClients.Interfaces
         Uri GetBaseURI();
         void SetBaseURI(string endpoint);
         Task<HttpResponseMessage> DeleteAsync(IAuthParams authParams, string endPoint, HttpContent content);
+        Task<HttpResponseMessage> DeleteAsync(IAuthParams authParams, string endPoint, HttpContent content, string callInitiator = null);
         Task<HttpResponseMessage> GetAsync(string requestUri);
         Task<HttpResponseMessage> GetAsync(IAuthParams auth0Auth, string requestUri);
+        Task<HttpResponseMessage> GetAsync(IAuthParams auth0Auth, string requestUri, string callInitiator = null);
         Task<HttpResponseMessage> PatchAsync(IAuthParams authParams, string endPoint, HttpContent content);
+        Task<HttpResponseMessage> PatchAsync(IAuthParams authParams, string endPoint, HttpContent content, string callInitiator = null);
         Task<HttpResponseMessage> PostAsync(IAuthParams authParams, string endPoint, HttpContent content);
+        Task<HttpResponseMessage> PostAsync(IAuthParams authParams, string endPoint, HttpContent content, string callInitiator = null);
         Task<HttpResponseMessage> PutAsync(IAuthParams authParams, string endPoint, HttpContent content);
+        Task<HttpResponseMessage> PutAsync(IAuthParams authParams, string endPoint, HttpContent content, string callInitiator = null);
         void SetContentType(string contentType);
         void AddDefaultRequestHeader(string key, string value);
     }
