57810e2c10aede75bdb9c88e8eb76327e394a08f
Andy Kratzert <andykratzert@quickenloans.com>
2021-07-26 17:15:21 -0400

    Fixing killswitch interfaces

--- Aether/Interfaces/Themis/IIdentifierRestriction.cs
+++ Aether/Interfaces/Themis/IIdentifierRestriction.cs
@@ -1,9 +1,16 @@
-﻿using Aether.Enums;
+﻿using System;
+using Aether.Enums;
 
 namespace Aether.Interfaces.Themis
 {
-    public interface IIdentifierRestriction : IRestriction
+    public interface IIdentifierRestriction
     {
         public IdentifierType IdentifierType { get; set; }
+        public string Id { get; set; }
+        public bool IsRestricted { get; set; }
+        public int CreatedById { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+        public int LastUpdatedById { get; set; }
     }
 }
--- Aether/Interfaces/Themis/ISystemRestriction.cs
+++ Aether/Interfaces/Themis/ISystemRestriction.cs
@@ -1,8 +1,16 @@
-﻿namespace Aether.Interfaces.Themis
+﻿using System;
+
+namespace Aether.Interfaces.Themis
 {
-    public interface ISystemRestriction : IRestriction
+    public interface ISystemRestriction
     {
         public string SystemId { get; set; }
         public string SystemName { get; set; }
+        public string Id { get; set; }
+        public bool IsRestricted { get; set; }
+        public int CreatedById { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+        public int LastUpdatedById { get; set; }
     }
 }
