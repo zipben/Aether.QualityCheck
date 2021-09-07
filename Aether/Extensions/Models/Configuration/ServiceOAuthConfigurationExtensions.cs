14743431c255733d0b72a8f8d17e05158710895a
Andy Kratzert <andykratzert@quickenloans.com>
2021-09-07 15:59:14 -0400

    Adding a Model for all OAuth service settings

---
+++ Aether/Extensions/Models/Configuration/ServiceOAuthConfigurationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Aether.Models.Configuration;
+using RockLib.OAuth;
+
+namespace Aether.Extensions.Models.Configuration
+{
+    public static class ServiceOAuthConfigurationExtensions
+    {
+        public static IAuthParams GetAuthParams(this ServiceOAuthConfiguration config) =>
+            new Auth0AuthParams(config.ClientId, config.ClientSecret, config.Audience, config.AuthorizerUrl);
+    }
+}
